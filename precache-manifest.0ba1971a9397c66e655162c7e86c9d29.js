882eade6384d4feb972a2e0e22785ee26ccb8e63
snyggme <rcbabahin@gmail.com>
2018-10-06 11:55:31 +0300

    Updates

---
+++ precache-manifest.0ba1971a9397c66e655162c7e86c9d29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "7a9c9697076202e3bfe4",
+    "url": "/tic-tac-toe/static/js/main.7a9c9697.chunk.js"
+  },
+  {
+    "revision": "51fcbc0b57ad45f04e61",
+    "url": "/tic-tac-toe/static/js/1.51fcbc0b.chunk.js"
+  },
+  {
+    "revision": "7a9c9697076202e3bfe4",
+    "url": "/tic-tac-toe/static/css/main.c4b65ce7.chunk.css"
+  },
+  {
+    "revision": "e4b3cd5906f72c60f9a3ad17a11fee12",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
